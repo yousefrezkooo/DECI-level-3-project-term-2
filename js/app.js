091b6e238310160dc7deec20fb5ab15ed4673fb7
Sarah Maris <sarah.maris@udacity.com>
2023-03-22 10:59:05 -0400

    Updates to add TODOs

--- js/app.js
+++ js/app.js
@@ -1,62 +1,7 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+/** TODO: Build the navigation bar using JavaScript **/
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+/** TODO: Add smooth scrolling **/
 
-/**
- * Define Global Variables
- * 
-*/
+/** TODO: Add an active state **/
 
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+/** TODO: Add a comment form **/
